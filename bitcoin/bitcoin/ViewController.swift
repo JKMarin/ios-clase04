b9750488acaf3509a69fdd42b140d7def834d79b
JKMarin <marin.stcruz@gmail.com>
2018-04-21 22:59:54 -0700

    creacion de proyecto bitcoin

---
+++ bitcoin/bitcoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitcoin
+//
+//  Created by user139236 on 4/21/18.
+//  Copyright © 2018 Juan C Marin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
